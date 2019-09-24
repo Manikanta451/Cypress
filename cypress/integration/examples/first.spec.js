775a05cc4709621acccca17a69c8fc8b2e9090b1
Manikanta <manikantat@kensium.com>
2019-09-24 17:42:43 +0530

    Validate Title

--- cypress/integration/examples/first.spec.js
+++ cypress/integration/examples/first.spec.js
@@ -1,15 +1,19 @@
 /// <reference types="Cypress" />
 
-describe('First Suite', function(){
+describe('First Suite', function () {
 
-it('First TestCase',function(){
+    it('First TestCase', function () {
 
-    cy.visit(Cypress.env("url"));
-    cy.get("input[ng-model='username']").type('admin');
-    cy.get("input[ng-model='password']").type('admin');
-    cy.get("button[type=submit]").click();
-    cy.title().should('eq', 'Doccept')
-    
-})
+        cy.visit(Cypress.env("url"));
+        cy.get("input[ng-model='username']").type('admin');
+        cy.get("input[ng-model='password']").type('admin');
+        cy.get("button[type=submit]").click();
+        cy.title().should('eq', 'Doccept')
 
+    })
+
+    it('validate title', function () {
+
+
+    })
 })
